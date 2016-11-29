add6b3bc2d08c7a2957583b1bcbea191dc15f352
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-11-29 15:56:53 +0100

    flashes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   #
   # get 'lessons/:id', to: 'lessons#show'
  resources :lessons, only: [:index, :show]
-  get 'users/new'
-
-  get 'users/create'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
