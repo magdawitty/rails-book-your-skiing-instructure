c7ee6a6132c20ce52cc8c5264963c2eab915f4a8
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-11-29 11:57:24 +0100

    models and controllers for users and lessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'lessons/index'
+
+  get 'lessons/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
