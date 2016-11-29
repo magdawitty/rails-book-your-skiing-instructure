da557d7fb8a61525084cae55543e370801d083bb
Giacomo Moscatelli <giacomom984@gmail.com>
2016-11-29 18:44:10 +0100

    More CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#email: ""
+#encrypted_password: ""
+
 user = [
   {
   name: "Leo Senti",
