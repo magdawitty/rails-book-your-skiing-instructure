ea9dab4872cd3411022bac9cf55c12e573027035
Dante Fereghetti <dante.fereghetti@gmail.com>
2016-11-30 11:59:49 +0100

    first step to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129114809) do
+ActiveRecord::Schema.define(version: 20161130102503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20161129114809) do
     t.date     "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "lesson_id"
+    t.index ["lesson_id"], name: "index_bookings_on_lesson_id", using: :btree
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 20161129114809) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "lessons"
 end
