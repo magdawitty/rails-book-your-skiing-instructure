53a531aed4963b76341a4dd0e752f0a9f3341343
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-11-30 13:19:08 +0100

    solved

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,10 @@ ActiveRecord::Schema.define(version: 20161130113513) do
     t.string   "experience_level"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-<<<<<<< HEAD
     t.string   "picture"
-=======
     t.integer  "user_id"
     t.integer  "price"
     t.index ["user_id"], name: "index_lessons_on_user_id", using: :btree
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
