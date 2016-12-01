986171345944a4d0ba2d323c3a77d6005493991a
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-12-01 11:27:48 +0100

    added new variable to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130114146) do
+ActiveRecord::Schema.define(version: 20161201102152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161130114146) do
     t.datetime "updated_at",                          null: false
     t.string   "avatar"
     t.integer  "stars"
+    t.boolean  "instructor"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
